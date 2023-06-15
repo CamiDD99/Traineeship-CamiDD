10450abadae8c1ce8b71a41c545e023bb02e498e
Cami De Decker <cami.de.decker@student.howest.be>
2023-06-15 16:43:41 +0200

    Edited some steps

--- STEP1/2a-Scaffolds.R
+++ STEP1/2a-Scaffolds.R
@@ -1,9 +1,6 @@
 ## Filtering the list of Mycogenomes
 ################################################################################
 # Downloading/loading packages
-pkg <- installed.packages()[, "Package"]
-if(!('dplyr' %in% pkg)) { install.packages("dplyr") }
-library(dplyr)
 library(xlsx)
 
 # Loading file
@@ -12,14 +9,12 @@ file <- read.csv("1d-allMyco_ncbigenome_overview3.csv", sep="\t", header=TRUE)
 
 # Retaining only those assemblies with < 10 scaffolds (3073 assemblies kept)
 scaf10 <- file[which(file$Scaffolds<10),]
-# Retaining only those assemblies with 1 scaffold (1157 assemblies kept)
-#scaf1 <- file[which(file$Scaffolds==1),]
 
-# Only retaining 1 (sub)species (with variant/strain) of the list with species 
-# < 10 scaffolds (1336 assemblies kept)
+
+# Only retaining a genome per (sub)species (with variant/strain) of the list 
+# with < 10 scaffolds (1336 assemblies kept)
 species10 <- distinct(scaf10, X.Organism.Name, .keep_all=TRUE)
-# Only retaining 1 species of the list with species = 1 scaffolds (412 assemblies kept)
-#species1 <- distinct(scaf1, X.Organism.Name, .keep_all=TRUE)
+
 
 # Saving in xlsx file
 write.xlsx(x = species10, 
